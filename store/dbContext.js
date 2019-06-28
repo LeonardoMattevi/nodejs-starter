a953a97d7acb6c683c412b0dd21d0d3b88a1cce1
Leonardo Mattevi <leonardo.mattevi@algorab.com>
2019-06-28 12:08:40 +0200

    new dbContext - reconnect when DB server goes down

--- store/dbContext.js
+++ store/dbContext.js
@@ -1,22 +1,78 @@
 const mongoose = require("mongoose");
-const connectionString = "mongodb://localhost/AuthProvider";
-mongoose.connect(connectionString, {useNewUrlParser: true})
-        .then(() => {
-          console.log("Connected to MongoDB");
-        })
-        .catch(err => {
-          console.error("Could not connect to MongoDB...", err);
-          throw new Error("Could not connect to MongoDB...", err);
-        });
-// schema
-const userSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  email: String,
-  isActive: Boolean,
-  modificationDate: { type: Date, default: null },
-  creationDate: { type: Date }
-});
-const _User = mongoose.model("User", userSchema);  
-// model
-exports.User = _User;
+const dbName = 'AuthProvider';
+const connectionString = "mongodb://localhost/";
+let _User = null;
+let db = null;
+
+// mongoose.connection.readyState:
+// 0: disconnected
+// 1: connected
+// 2: connecting
+// 3: disconnecting
+async function connect() {
+  try {    
+    if(mongoose.connection.readyState === 1)
+        await mongoose.connection.close();
+
+    await mongoose.connect(connectionString + dbName, {useNewUrlParser: true});
+    console.log("Connected to Mongoose");
+    
+    createSchemas();
+  } catch(err) {
+    console.error("Could not connect to Mongoose...", err);
+  };
+}
+function createSchemas() {
+  // schema
+  if(_User)
+    return;
+
+  db = mongoose.connection;
+  db.on('connecting', function() {
+    console.log('connecting to MongoDB...');
+  });
+  db.on('error', function(error) {
+    console.error('Error in MongoDb connection: ' + error);
+  });
+  db.on('connected', function() {
+    console.log('MongoDB connected!');
+  });
+  db.once('open', function() {
+    console.log('MongoDB connection opened!');
+  });
+  db.on('reconnected', function () {
+    console.log('MongoDB reconnected!');
+  });
+  db.on('disconnected', function() {
+    console.log('MongoDB disconnected!');
+  });
+
+  const userSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+    isActive: Boolean,
+    modificationDate: { type: Date, default: null },
+    creationDate: { type: Date }
+  });
+  _User = mongoose.model("User", userSchema); 
+}
+connect();
+let canTryToReconnect = true;
+module.exports = {
+  get User() {
+    if (_User && mongoose.connection.readyState === 1)
+      return _User;
+      
+
+    // if the DB connection is down, it can try to reconnect 1 time only every 2 seconds
+    if (canTryToReconnect) {
+      canTryToReconnect = false;
+      setTimeout(function() {
+        connect();
+        canTryToReconnect = true;
+      }, 2000);
+    }
+    throw "the DB connection is closed.";
+  }  
+};

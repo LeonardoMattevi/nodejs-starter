c34fe3f5d061e37a469db402e25b5b43c84097f8
LeonardoMattevi <leonardo.mattevi@gmail.com>
2020-05-03 23:47:54 +0200

    fix morgan

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+if(config.enableAccessLogs) {
+  app.use(morgan("tiny"));
+}
 app.use(helmet());
 app.use(express.static("public"));
 
 require("./routes/index")(app);
-
-if (app.get("env") === "development") {
-  app.use(morgan("tiny"));
-}
 
 app.listen(port, () => {
   console.log(`listening on port ${port}...`);

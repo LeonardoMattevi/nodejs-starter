6c363300c8ad3818de561a25efdaaa92d1871d86
LeonardoMattevi <leonardo.mattevi@gmail.com>
2020-05-06 00:24:23 +0200

    fix services e validators, api-calls.http added

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,8 @@ const { logger } = require('../core/logger');
 const { ADMIN, BASE } = require('../core/roles');
 const authProvider = require("../middleware/authProvider");
 const usersService = require("../services/usersService");
-const validators = require("../store/validators");
+const { validateCreateUser, validateEditUser } = require("../store/validators");
+const HttpError = require("../store/models/common-models");
 
 router.get("/", authProvider([ADMIN, BASE]), async (req, res) => {
   logger.info("You get all users!");
@@ -14,34 +15,39 @@ router.get("/", authProvider([ADMIN, BASE]), async (req, res) => {
 router.get("/:id", authProvider([ADMIN, BASE]), async (req, res) => {
   let result = await usersService.getUser(req.params.id);
   if(!result)
-    throw ({ httpCode: 404, message: "The user with the given ID was not founded."});
+    throw new HttpError("The user not found.", 404);
     
   res.send(result);
 });
 router.post("/", authProvider([ADMIN]), async (req, res) => {
-  const { error } = validators.validateUser(req.body);
+  const { error } = validateCreateUser(req.body);
   if (error) 
-    throw ({ httpCode: 400, message: error.details[0].message});
+    throw new HttpError(error.details[0].message, 400);
   
   let result = await usersService.createUser(req.body);
   res.send(result);
 });
 router.put("/:id", authProvider([ADMIN]), async (req, res) => {
+  const { error } = validateEditUser(req.body);
+  if (error)
+    throw new HttpError(error.details[0].message, 400);
+  
+  if (req.body._id != req.params.id)
+    throw new HttpError("The body _id does not match with URL ID", 400);
+  
   let result = await usersService.getUser(req.params.id);
   if (!result)
-    throw ({ httpCode: 404, message: "The user with the given ID was not founded."});
-
-  const { error } = validators.validateUser(req.body);
-  if (error)
-    throw ({ httpCode: 400, message: error.details[0].message});
+    throw new HttpError("The user not found.", 404);
 
   result = await usersService.updateUser(req.params.id, req.body);
+  if(result.ok)
+    result = await usersService.getUser(req.params.id);
   res.send(result);
 });
 router.delete("/:id", authProvider([ADMIN]), async (req, res) => {
   let result = await usersService.getUser(req.params.id);
   if (!result)
-    throw ({ httpCode: 404, message: "The user with the given ID was not founded." });
+    throw new HttpError("The user not found.", 404);
 
   result = await usersService.deleteUser(req.params.id);
   res.send(result);

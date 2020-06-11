93f1f811935ded8d22d605811eb08bbd4ceda22c
Leonardo Mattevi <leonardo.mattevi@gmail.com>
2020-06-11 11:49:38 +0200

    test fixed

--- tests/core.spec.js
+++ tests/core.spec.js
@@ -1,10 +1,10 @@
-const coreUsers = require("../core/users");
+const { checkEmail } = require("../core/users");
 
 describe("Core users functions validators", () => {
     it("Check email function", () => {
-      expect(coreUsers.checkEmail("leonardo.mattevi@email.it")).toBe(true);
-      expect(coreUsers.checkEmail("leonardo.mattevi@email")).toBe(false);
-      expect(coreUsers.checkEmail("leonardo.matteviemail.it")).toBe(false);
+      expect(checkEmail("leonardo.mattevi@email.it")).toBe(true);
+      expect(checkEmail("leonardo.mattevi@email")).toBe(false);
+      expect(checkEmail("leonardo.matteviemail.it")).toBe(false);
     });
   });
   
--- tests/validators.spec.js
+++ tests/validators.spec.js
@@ -1,15 +1,15 @@
-const { validateUser } = require("../store/validators");
+const { validateCreateUser } = require("../store/validators");
 
 describe("Validators Joy", () => {
   it("Validate User", () => {
-    expect(!!validateUser({
+    expect(!!validateCreateUser({
         firstName: '',
         lastName: 'test',
         email: 'test@test.it',
         isActive: true
     }).error).toBe(true);
     
-    expect(!!validateUser({
+    expect(!!validateCreateUser({
         firstName: 'test',
         lastName: 'test',
         email: 'test@test.it',

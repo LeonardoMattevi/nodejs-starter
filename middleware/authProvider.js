9333e487cd559e18cd89c2d473f64c7ba287bd0b
LeonardoMattevi <leonardo.mattevi@gmail.com>
2020-08-09 22:38:57 +0200

    res.locals.authInfo from express docs

--- middleware/authProvider.js
+++ middleware/authProvider.js
@@ -5,26 +5,26 @@ const authentication = function () {
     const JWT_KEY = ""; // insert here JWT key;
     return function getUser(req, res, next) {
         let token = req.headers['authorization'] || req.headers['x-access-token']; 
-        req.authUser = null;
+        res.locals.authInfo = null;
         console.log('authentication...', token || 'unknow user');
         
         if(token) {
             try {
                 decoded = jwt.verify(token, JWT_KEY);
-                req.authUser = { ...decoded, JWT: token };
+                res.locals.authInfo = { ...decoded, JWT: token };
             } catch(err) {
                 throw new HttpError(err, 403);
-            }    
+            }   
         }
         next();
     }
 };
 const authorization = function (routeValidRoles) {
     return function getAccess(req, res, next) {
-        if(!req.authUser) {
+        if(!res.locals.authInfo) {
             throw new HttpError("User is not authenticated.", 401);
         }
-        if(!routeValideRoles.includes(req.authUser.roleName)) {
+        if(!routeValideRoles.includes(res.locals.authInfo.roleName)) {
             throw new HttpError("User does not have the right roles.", 401);
         }
         console.log('authorization...', routeValidRoles);
